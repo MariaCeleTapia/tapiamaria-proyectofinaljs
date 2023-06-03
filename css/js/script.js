3b2948f5d58447be3c1e0c21c77e80b7ddc07946
MariaCeleTapia <marice.tap16@gmail.com>
2023-06-02 21:59:07 -0600

    cambio de carpeta del proyecto

---
+++ css/js/script.js
@@ -0,0 +1,46 @@
+//PRODUCTOS
+
+const productos = [
+    //Anillos
+    {
+        id: "anillos-01",
+        titulo: "Anillo 01",
+        imagen: "./img/anillos/01.jpg",
+            catergoria: {
+        nombre: "anillos",
+        id: "anillos"
+    },
+    precio: 900
+    },
+{
+    id: "Anillo-02",
+    titulo: "Anillo 02",
+    imagen: "./img/anillos/02.jpg",
+        catergoria: {
+    nombre: "Anillos",
+    id: "anillos"
+    },
+    precio: 900
+    },
+{
+    id: "Anillo-03",
+        titulo: "Anillo 03",
+            imagen: "./img/anillos/03.jpg",
+    catergoria: {
+        nombre: "Anillos",
+            id: "anillos"
+    },
+    precio: 900
+    },
+{
+    id: "Anillo-04",
+        titulo: "Anillo 04",
+            imagen: "./img/anillos/04.jpg",
+    catergoria: {
+        nombre: "Anillos",
+            id: "anillos"
+    },
+    precio: 900
+    },
+
+]
