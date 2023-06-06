eb936e106878cfc768201072f68aa86a89bf2baa
MariaCeleTapia <marice.tap16@gmail.com>
2023-06-06 06:06:49 -0600

    modificacion js

---
+++ js/inicio.js
@@ -0,0 +1,24 @@
+//carrousel
+
+const grande = document.querySelector(".grande");
+const punto = document.querySelectorAll(".punto");
+
+punto.forEach((cadaPunto , i) =>{
+    punto[i].addEventListener("click" , (i) =>{
+
+        let posicion = i;
+
+        let operacion = posicion * -50;
+
+        grande.style.transform = `translateX(${operacion}%)`;
+
+        punto.forEach( (cadaPunto , i )=>{
+
+            punto[i].classList.remove("active");
+        })
+        punto[i].classList.add("active");
+    })
+})
+
+
+
--- js/script.js
+++ js/script.js
@@ -3,177 +3,205 @@
 const productos = [
     //Anillos
     {
-        id: "Anillos-01",
+        id: "anillos-01",
         titulo: "Anillo 01",
         imagen: "./img/anillos/01.jpg",
         categoria: {
             nombre: "Anillo",
             id: "anillos"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Anillos-02",
+        id: "anillos-02",
         titulo: "Anillo 02",
         imagen: "./img/anillos/02.jpg",
         categoria: {
             nombre: "Anillo",
             id: "anillos"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Anillos-03",
+        id: "anillos-03",
         titulo: "Anillo 03",
         imagen: "./img/anillos/03.jpg",
         categoria: {
             nombre: "Anillo",
             id: "anillos"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Anillos-04",
+        id: "anillos-04",
         titulo: "Anillo 04",
         imagen: "./img/anillos/04.jpg",
         categoria: {
             nombre: "Anillo",
             id: "anillos"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Anillos-05",
+        id: "anillos-05",
         titulo: "Anillo 05",
         imagen: "./img/anillos/05.jpg",
         categoria: {
             nombre: "Anillo",
             id: "anillos"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Anillos-06",
+        id: "anillos-06",
         titulo: "Anillo 06",
         imagen: "./img/anillos/06.jpg",
         categoria: {
             nombre: "Anillo",
             id: "anillos"
-        }
+        },
+        precio: 900
     },
     //Aros
     {
-        id: "Aros-01",
+        id: "aros-01",
         titulo: "Aros 01",
         imagen: "./img/aros/01.jpg",
         categoria: {
             nombre: "Aros",
             id: "aros"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Aros-02",
+        id: "aros-02",
         titulo: "Aros 02",
         imagen: "./img/aros/02.jpg",
         categoria: {
             nombre: "Aros",
             id: "aros"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Aros-02",
+        id: "aros-02",
         titulo: "Aros 02",
         imagen: "./img/aros/02.jpg",
         categoria: {
             nombre: "Aros",
             id: "aros"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Aros-03",
+        id: "aros-03",
         titulo: "Aros 03",
         imagen: "./img/aros/03.jpg",
         categoria: {
             nombre: "Aros",
             id: "aros"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Aros-04",
+        id: "aros-04",
         titulo: "Aros 04",
         imagen: "./img/aros/04.jpg",
         categoria: {
             nombre: "Aros",
             id: "aros"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Aros-05",
+        id: "aros-05",
         titulo: "Aros 05",
         imagen: "./img/aros/05.jpg",
         categoria: {
             nombre: "Aros",
             id: "aros"
-        }
+        },
+        precio: 900
     },
     //Cadenas
     {
-        id: "Cadenas-01",
+        id: "cadenas-01",
         titulo: "Cadena 01",
         imagen: "./img/cadenas/01.jpg",
         categoria: {
             nombre: "Cadenas",
-            id: "cadena"
-        }
+            id: "cadenas"
+        },
+        precio: 900
     },
     {
-        id: "Cadenas-02",
+        id: "cadenas-02",
         titulo: "Cadena 02",
         imagen: "./img/cadenas/02.jpg",
         categoria: {
             nombre: "Cadenas",
-            id: "cadena"
-        }
+            id: "cadenas"
+        },
+        precio: 900
     },
     {
-        id: "Cadenas-03",
+        id: "cadenas-03",
         titulo: "Cadena 03",
         imagen: "./img/cadenas/03.jpg",
         categoria: {
             nombre: "Cadenas",
-            id: "cadena"
-        }
+            id: "cadenas"
+        },
+        precio: 900
     },
     //Pulseras
     {
-        id: "Pulseras-01",
+        id: "pulseras-01",
         titulo: "Pulsera 01",
         imagen: "./img/pulseras/01.jpg",
         categoria: {
             nombre: "Pulseras",
             id: "pulseras"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Pulseras-02",
+        id: "pulseras-02",
         titulo: "Pulsera 02",
         imagen: "./img/pulseras/02.jpg",
         categoria: {
             nombre: "Pulseras",
             id: "pulseras"
-        }
+        },
+        precio: 900
     },
     {
-        id: "Pulseras-03",
+        id: "pulseras-03",
         titulo: "Pulsera 03",
         imagen: "./img/pulseras/03.jpg",
         categoria: {
             nombre: "Pulseras",
             id: "pulseras"
-        }
+        },
+        precio: 900
     },
 
 ];
+
 const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numero = document.querySelector("#numero");
 
-function cargarProductos() {
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
 
-    productos.forEach(producto => {
+function cargarProductos(productosElegidos) {
+    contenedorProductos.innerHTML ="";
+    
+    productosElegidos.forEach(producto => {
         
         const div = document.createElement("div");
         div.classList.add("producto");
@@ -187,8 +215,69 @@ function cargarProductos() {
         `;
 
         contenedorProductos.append(div);
-
     })
+
+    actualizarBotonesAgregar();
 }
 
-cargarProductos();
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id ===e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText ="Todos los productos";
+            cargarProductos(productos);
+        }
+    })
+});
+
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumero();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+    
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+    actualizarNumero();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+} 
+
+function actualizarNumero() {
+    let nuevoNumero = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numero.innerText = nuevoNumero;
+}
+
+
